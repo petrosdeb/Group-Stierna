a00dc8d9010492a8e27c8aecca0f79901cc63463
petrosdebesay <petrosd@student.chalmers.se>
2017-09-26 10:56:18 +0200

    Basci ACC code structure
    Yet to be completed, only basic structure defined

--- Code/src/platooning/ACCController.java
+++ Code/src/platooning/ACCController.java
@@ -1,5 +1,61 @@
 package platooning;
 
-public class ACCController {
+import sics.plugin.PlugInComponent;
+import sics.port.PluginPPort;
+import sics.port.PluginRPort;
 
+public class ACCController extends PlugInComponent {
+	
+	public PluginPPort writeSpeed;
+	public PluginPPort writeSteer;
+	public PluginPPort writeLED;
+	public PluginRPort readFrontSpeed;
+	public PluginRPort readRearSpeed;
+	public PluginRPort readPosition;
+	
+	
+	public ACCController(String[] args) {
+		super(args);
+	}
+
+	public static void main(String[] args) {
+		
+		ACCController controller = new ACCController(args);
+		controller.run();
+		
+	}
+
+	@Override
+	public void run() {
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void sendToSCU(int data) {
+		
+	}
+
+}
+
+class SCUData extends Thread {
+	private PluginRPort scuPort;
+	private Integer data;
+	
+	public SCUData(PluginRPort scuPort) {
+		this.scuPort = scuPort;
+	}
+	
+	public void run() {
+		data = (Integer) scuPort.read(); //might return a string
+	}
+	
+	public Integer getData() {
+		return data;
+	}
+	
 }
--- Code/src/platooning/ControllerSCU.java
+++ Code/src/platooning/ControllerSCU.java
@@ -1,5 +1,28 @@
 package platooning;
 
-public class ControllerSCU {
+import sics.plugin.PlugInComponent;
+import sics.port.PluginRPort;
+
+public class ControllerSCU extends PlugInComponent {
+	
+	public PluginRPort readDistance;
+	public PluginRPort readIMU;
+	public PluginRPort rportFront;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void sendToVCU(int data) {
+		
+	}
 
 }
