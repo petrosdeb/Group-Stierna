7cfad210dec1a4a0ac64c961dd3a1767bad62828
Emil Logren <Emil@Emils-MacBook-Pro.local>
2017-10-04 15:21:05 +0200

    Add untested ACC

--- acc.py
+++ acc.py
@@ -16,6 +16,8 @@ break_distance = {21 : 8,
 update_time = 0.1
 acceleration_interval = 10
 limit_d = 2
+sigma_v = 2
+sigma_d = 0.1
 
 
 # adjust speed according to distance to preceding vehicle, to match its speed 
@@ -58,53 +60,75 @@ def on():
 			drive(sp)
 		print("Distance, speed: ", d, ", ", sp)
 
-
+# Behaviour for Adaptive Cruise Control (ACC). set_sp is desired speed to be maintained.
 def acc(set_sp):
+	max_speed = set_sp
+	max_d = calculate_break_distance(max_speed)
 	while True:
-		max_speed = set_sp
-		max_d = calculate_break_distance(max_speed)
 		v = g.outspeedcm/2  # last input to drive()
-		acceleration_speed = max_speed/acceleration_interval
-		while get_d() >= max_d and v < max_speed:
-			v = v + acceleration_speed
-			accelerate(v)
-		if get_d() < max_d:
-			delta_d = get_delta_d()
-			delta_v = get_delta_v(delta_d, update_time)
-			preceding_v = v + delta_v
+		acceleration_speed = (max_speed-v)/acceleration_interval  # accelerate with intervals 
+		while is_outside_break_distance(max_d) and v < max_speed:
+			accelerate(acceleration_speed)
+			v = g.outspeedcm/2
+		if not is_outside_break_distance(max_d):
+			v = g.outspeedcm/2
+			preceding_v = calculate_preceding_speed(v)
 			break_distance = calculate_break_distance(preceding_v)
-			acceleration_speed = preceding_v/acceleration_interval
-			while get_d > break_distance and v > preceding_v:
-				v = v + acceleration_speed
-				accelerate(v)
+			acceleration_speed = (preceding_v-v)/acceleration_interval
+			if is_outside_break_distance(break_distance) and is_match_speed(preceding_v):
+				drive(v)
+			else:
+				while not is_outside_break_distance(break_distance) and is_match_speed(preceding_v):
+					adjust_distance(break_distance)
+				while not is_match_speed(preceding_v):
+					accelerate(acceleration_speed)
+					v = g.outspeedcm/2
 
 
-
-
-
-
-#divide delta_v into even pieces, add these to current speed
-def accelerate(delta_v, v):
+# Divide delta_v into even pieces, add these to current speed
+def accelerate(delta_v):
+	v = g.outspeedcm/2 # Current speed
 	drive(v+delta_v)
 	
-#Gets difference in distance to preceding vehicle given a time
+# Gets difference in distance to preceding vehicle given a time
 def get_delta_d():
 	d0 = get_d()
 	time.sleep(update_time)
 	d1 = get_d()
 	return d1-d0
 
-#Gets distance to preceding vehicle, if not more than 2
+# Gets distance to preceding vehicle, if not more than 2
 def get_d():
 	d = min(g.can_ultra, 2)
 	return d
 
-#Calculates speed of preceding vehicle
-def get_delta_v(delta_d, t):
-	delta_v = delta_d/t
+# Calculates difference in speed of preceding vehicle compared to MOPED
+def get_delta_v(delta_d):
+	delta_v = delta_d/update_time
 	return delta_v
 
-#Calculates accepted break distance according to given speed
+# Calculates accepted break distance according to given speed
 def calculate_break_distance(sp):
 	return 0.06*(sp(**1.6))
 
+# Returns True if MOPED is matching speed to preceding vehicle 
+def is_match_speed(sp):
+	v = g.outspeedcm/2
+	return sp - sigma_v <= v and v <= sp
+
+# Returns True if MOPED is outside of break distance to preceding vehicle 
+def is_outside_break_distance(break_distance):
+	return get_d() >= break_distance + sigma_d
+
+# Adjusts speed of vehicle until set distance is kept
+def adjust_distance(break_distance):
+	delta_d = get_d() - break_distance
+	delta_v = get_delta_v(delta_d)
+	acceleration_speed = delta_v/acceleration_interval
+	accelerate(acceleration_speed)
+
+def calculate_preceding_speed(v):
+	delta_d = get_delta_d()
+	delta_v = get_delta_v(delta_d)
+	preceding_v = v + delta_v
+	return preceding_v
