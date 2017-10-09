97d50f21452f46772b80746e63d4b578a1cdecbb
LatteG <lahtig@student.chalmers.se>
2017-10-09 15:52:03 +0200

    Switches to using an AsyncTask instead of Thread for socket handling. Can now send data properly.

--- StiernaController/app/src/main/java/groupstierna/stiernacontroller/MainActivity.java
+++ StiernaController/app/src/main/java/groupstierna/stiernacontroller/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
-    private StiernaConnector stiernaConnector;
 
     private TextView mTextMessage;
     private Keyword mode = Keyword.MANUAL;
@@ -25,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewManualSpeedDisplay;
     private TextView textViewACCSpeedDisplay;
 
-    private boolean connectionStatus = false;
+    private String hostName = "192.168.0.0";
+    private int portNumber = 9000;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -61,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
             updateConnection();
         }
     };
-
 
     private SeekBar.OnSeekBarChangeListener manualSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
@@ -115,8 +114,6 @@ public class MainActivity extends AppCompatActivity {
         textViewManualSpeedDisplay = (TextView) findViewById(R.id.textViewManualSpeedDisplay);
         textViewACCSpeedDisplay = (TextView) findViewById(R.id.textViewACCSpeedDisplay);
 
-        stiernaConnector = new StiernaConnector();
-
         setSeekBarDefaultValues();
 
         radioGroup.setOnCheckedChangeListener(radioGroupChangeListener);
@@ -160,25 +157,21 @@ public class MainActivity extends AppCompatActivity {
                 return false;
         }
 
-        if (!stiernaConnector.send(mode.getMessage())) {
-            updateConnectionStatus(false);
+        trySend(mode.getMessage());
+        if (mode == Keyword.ACC) {
+            updateSpeed();
         }
         return true;
     }
 
     private void updateConnection() {
-        stiernaConnector.setHostName(editTextIPNumber.getText().toString());
-        stiernaConnector.setPortNumber(Integer.valueOf(editTextPortNumber.getText().toString()));
-        updateConnectionStatus(stiernaConnector.tryConnect());
+        hostName = editTextIPNumber.getText().toString();
+        portNumber = Integer.valueOf(editTextPortNumber.getText().toString());
+        updateConnectionStatus();
     }
 
-    private void updateConnectionStatus(boolean status) {
-        connectionStatus = status;
-        if (connectionStatus) {
-            textViewConnectionStatus.setText(R.string.connected);
-        } else {
-            textViewConnectionStatus.setText(R.string.disconnected);
-        }
+    private void updateConnectionStatus() {
+        trySend("");
     }
 
     public void updateControl() {
@@ -196,15 +189,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             speed = Integer.toString(seekBarACCSpeed.getProgress() - 100);
         }
-        if (!stiernaConnector.send(Keyword.DRIVE.getMessage() + " " + speed)) {
-            updateConnectionStatus(false);
-        }
+        trySend(Keyword.DRIVE.getMessage() + " " + speed);
     }
 
     private void updateSteering() {
         String steering = Integer.toString(seekBarSteering.getProgress() - 100);
-        if (!stiernaConnector.send(Keyword.STEER.getMessage() + " " + steering)) {
-            updateConnectionStatus(false);
-        }
+        trySend(Keyword.STEER.getMessage() + " " + steering);
+    }
+
+    private void trySend(String message) {
+        StiernaAsyncClient client = new StiernaAsyncClient(hostName, portNumber, message, textViewConnectionStatus);
+        client.execute();
     }
 }
---
+++ StiernaController/app/src/main/java/groupstierna/stiernacontroller/StiernaAsyncClient.java
@@ -0,0 +1,57 @@
+package groupstierna.stiernacontroller;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class StiernaAsyncClient extends AsyncTask<Void, Void, TextView> {
+    private Boolean successful;
+    private String hostName;
+    private int portNumber;
+    private String message;
+    private TextView connectionStatus;
+
+    public StiernaAsyncClient(String hostName, int portNumber, String message, TextView connectionStatus) {
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+        this.message = message;
+        this.connectionStatus = connectionStatus;
+    }
+
+    @Override
+    protected TextView doInBackground(Void... voids) {
+        Socket socket;
+        PrintWriter out;
+
+        InetSocketAddress address = new InetSocketAddress(hostName, portNumber);
+
+        try {
+            socket = new Socket();
+            socket.connect(address);
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            successful = false;
+            return null;
+        }
+
+        if (!message.isEmpty()) {
+            out.println(message);
+        }
+
+        successful = true;
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(TextView result) {
+        if (successful) {
+            connectionStatus.setText(R.string.connected);
+        } else {
+            connectionStatus.setText(R.string.disconnected);
+        }
+    }
+}
