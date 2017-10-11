8c0dd8c81a45dcf2ad71eca055465fb22424b4db
petrosdebesay <petrosd@student.chalmers.se>
2017-10-11 20:07:23 +0200

    ACC partially integrated with Controller

--- lateralControl/lateralControl.py
+++ lateralControl/lateralControl.py
@@ -6,39 +6,44 @@ LATERALERROR = 10
 
 #Accepted horizontal positioning distance
 
-def __init__():
-    main()
+class LateralControl:
 
-def main():
-    while true:
-        capture()
-        image = misc.imread('current-image.jpg')
-        image = image.astype('float32')
-        adapt_steering(LN.find(image))
+    def __init__():
+        main()
 
-def capture():
-    camera = picamera.PiCamera()
-    camera.resolution = (1024, 768)
-    camera.capture('current-image.jpg')
+    def main():
+        while true:
+            capture()
+            image = misc.imread('current-image.jpg')
+            image = image.astype('float32')
+            adapt_steering(LN.find(image))
 
-def adapt_steering(position):
-    while not position+LATERALERROR> position and position>position-LATERALERROR
-        if position >LATERALERROR
-            steerfactor = position /10
-            if steerfactor > 100
-                steerfactor = 100
-            else if steerfactor < -100
-                steerfactor = -100
-            else if steerfactor < 1 and steerfactor > 0
-                steerfactor =    1
-            else if steerfactor > -1 and steerfactor < 0
-                steerfactor = 1
-        CRL_CAR.steer(steerfactor)
+    def capture():
+        camera = picamera.PiCamera()
+        camera.resolution = (1024, 768)
+        camera.capture('current-image.jpg')
+
+    def adapt_steering(position):
+        while not position+LATERALERROR> position and position>position-LATERALERROR
+            if position >LATERALERROR
+                steerfactor = position /10
+                if steerfactor > 100
+                    steerfactor = 100
+                else if steerfactor < -100
+                    steerfactor = -100
+                else if steerfactor < 1 and steerfactor > 0
+                    steerfactor =    1
+                else if steerfactor > -1 and steerfactor < 0
+                    steerfactor = 1
+            CRL_CAR.steer(steerfactor)
 
 
-def receive_edge_position():
-    pass
+    def receive_edge_position():
+        pass
 
-def get_steer():
-    pass
+    def should_steer():
+        pass
+
+    def get_steer():
+        pass
 
