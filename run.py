6d9fb51edb49cd093f69fb52af02daa70abe319e
petrosdebesay <petrosd@student.chalmers.se>
2017-10-12 21:31:07 +0200

    Simple ACC test

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ from nav1 import whole4, pause, cont
 from driving import stop, drive, steer
 from constant_speed import constant_speed
 from acc import activate_acc, on, acc, acc_on
+from extremely_simple_acc_test import acc_test
 
 init()
