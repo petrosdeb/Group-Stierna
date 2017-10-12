9056102278088ed853ba77956f3bcb594e6e90f3
Jesper Holm <holmje@student.chalmers.se>
2017-10-12 14:16:08 +0200

    Updated take_picture
    imported take_picture

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ from nav1 import whole4, pause, cont
 from driving import stop, drive, steer
 from constant_speed import constant_speed
 from acc import activate_acc, on, acc, acc_on
+from take_picture import test_steer
 
 init()
