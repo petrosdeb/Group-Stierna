6751cdf0262e3239ca79a49c1ef0962534a3e5e2
petrosdebesay <petrosd@student.chalmers.se>
2017-10-16 12:47:46 +0200

    Added conditions so simple_acc

--- simple_acc.py
+++ simple_acc.py
@@ -1,10 +1,18 @@
 '''
 acc using relative speed
+
+issue 1. Do we need to set g.braking = True when we are braking?
+
+The reaction time is 0,25 seconds. Meaning the distance to the preceding car has to be at least speed/4.
+If the car is driving 3 m/s, then the distance has to be at least ,75 meters. However the sensor can
+detect distances of up to 3,7 meters, so initially we should set distance to be greater than ,75 meters.
 '''
 
 import time
 from driving import drive
 from nav import *
+
+
 
 #use_electric = True to use electric brake
 def acc_run_test(use_electric):
@@ -15,7 +23,7 @@ def acc_run_test(use_electric):
         distance = g.can_ultra
         relative_speed = calc_relative_speed(prev_distance, distance, 0.25)
         
-        if relative_speed >= 0: #increase speed, but for now only set to 19
+        if relative_speed >= 0 and distance > 1: #increase speed, but for now only set to 19
             drive(19)
         else:
             if use_electric:
