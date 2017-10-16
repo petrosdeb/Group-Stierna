5b4d1f04e3014960f6ec66903393b7053d142ba3
petrosdebesay <petrosd@student.chalmers.se>
2017-10-16 22:28:53 +0200

    Fixed merge conflicts

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from driving import stop, drive, steer
 from constant_speed import constant_speed
 from acc import activate_acc, on, acc, acc_on
 from extremely_simple_acc_test import acc_test
+from simple_acc import acc_run_test
 from take_picture import test_steer
 
 init()
