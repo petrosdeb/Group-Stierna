b9b24cd5fb20d108818fc95aff7f5a1d8f65c8ee
Gustav Lahti <gustav.lahti@gmail.com>
2017-10-18 21:12:15 +0200

    Logic fixing cont + print -> logging

--- onMoped/acc.py
+++ onMoped/acc.py
@@ -3,14 +3,17 @@ import time
 from _thread import start_new_thread
 from math import ceil, floor
 
+from pip.utils import logging
+
 
 class Acc():
-    DISTANCE_CAP = 3
+    DISTANCE_CAP_METRES = 3
+    DISTANCE_CAP_CENTIMETRES = DISTANCE_CAP_METRES * 100
     SAFE_DISTANCE = 40
     WANTED_DISTANCE = 60
-    DECELERATION_RATIO = 1 / 10
-    ACCELERATION_RATIO = 1 / 50
-    MAX_TIME_PASSED = 0.5
+    DECELERATION_FACTOR = -1.5
+    ACCELERATION_FACTOR = 0.5
+    MAX_TIME_PASSED = 1
 
     def __init__(self, core):
         self.distance_list = []
@@ -24,7 +27,7 @@ class Acc():
         start_new_thread(self.__acc_on, ())
 
     def __acc_on(self):
-        print("started acc_on")
+        logging.info("Started acc_on")
 
         last_time = time.time()
 
@@ -32,31 +35,36 @@ class Acc():
 
             delta_d = self.__get_d()
 
-            c_time = int(time.time())
-            if c_time % 3 == 0 and c_time != last_time:
-                print(str(c_time) + ': ' + type(self).__name__ + ' suggest driving at ' + str(self.speed))  # usch
-                last_time = c_time
-                print("Distance: " + str(delta_d))
-                print("Acc debug string: " + self.debug_string)
-
             # If we have no value, set speed to 0 as a safety measure
-            if delta_d is None:
-                self.speed = 0
-                continue
-            # print("delta_d = " + '%.2f' % delta_d)
+
             delta_v = self.__get_delta_v_for_forward_object()
 
-            if delta_d >= self.DISTANCE_CAP:
+            c_time = int(time.time())
+            if c_time % 3 == 0 and c_time != last_time:
+                logging.info("{} : {} suggest speed : {} | d_dist : {} | d_velo : {} | dbg : {}".format(
+                    c_time,
+                    type(self).__name__,
+                    self.speed,
+                    delta_d,
+                    delta_v,
+                    self.debug_string
+                ))
+
+            if delta_d is None or delta_v is None:
+                self.speed = 0
+                continue
+
+            if delta_d >= self.DISTANCE_CAP_CENTIMETRES:
                 self.debug_string = "NO DETECTED TARGET"
                 # in case of no obstacles, go for desired speed
                 if self.core.speed != self.wanted_speed:
                     self.speed = self.wanted_speed
             elif delta_d < self.SAFE_DISTANCE:  # decrease speed if too close to target
                 self.debug_string = "TOO CLOSE TO TARGET"
-                self.__change_speed(-1 + (delta_d - self.SAFE_DISTANCE) * self.DECELERATION_RATIO)
+                self.__change_speed(-1 + (delta_d - self.SAFE_DISTANCE) * self.DECELERATION_FACTOR)
             elif delta_d > self.WANTED_DISTANCE:  # increase speed if too far away from target
                 self.debug_string = "NOT CLOSE ENOUGH TO TARGET"
-                self.__change_speed(1 + (delta_d - self.WANTED_DISTANCE) * self.ACCELERATION_RATIO)
+                self.__change_speed(1 + (delta_d - self.WANTED_DISTANCE) * self.ACCELERATION_FACTOR)
             elif delta_v < 0:  # decrease speed if moving too fast relative to target
                 self.debug_string = "TOO FAST"
                 self.__change_speed(delta_v)
@@ -67,18 +75,19 @@ class Acc():
                 self.debug_string = "NOTHING HAPPENS"
 
     def __change_speed(self, delta_v):
-        output = self.core.speed
+        previous_output = float(self.core.speed)
+        delta_v = float(delta_v)
         # Should prevent MOPED from backing when "braking" at current speed = 0
-        if output == 0 and delta_v < 0:
+        if previous_output == 0 and delta_v < 0:
             self.speed = 0
         else:
-            if abs(output + delta_v) > 100:
-                if output + delta_v < 0:
+            if abs(previous_output + delta_v) > 100:
+                if previous_output + delta_v < 0:
                     self.speed = -100
                 else:
                     self.speed = 100
             else:
-                self.speed = output + delta_v
+                self.speed = previous_output + delta_v
 
     # Gets distance to preceding vehicle, if not more than 2
     def __get_d(self):
@@ -88,27 +97,26 @@ class Acc():
         try:
             (timestamp, dist) = self.core.get_ultra_data()[0]
         except IndexError:
-            return None # None values are handled in the loop
+            return None  # None values are handled in the loop
 
         if len(self.distance_time_list) is 0:
             self.__update_lists(timestamp, dist)
         elif timestamp != self.distance_time_list[-1]:
             self.__update_lists(timestamp, dist)
 
-        return self.__get_average_of(self.distance_list) * 100
+        return self.__get_average_of(self.distance_list)
 
     def __update_lists(self, timestamp, dist):
-        self.distance_list.append(min(dist, self.DISTANCE_CAP))
+        self.distance_list.append(min(dist, self.DISTANCE_CAP_CENTIMETRES))
         self.distance_time_list.append(timestamp)
 
-        size = len(self.distance_list)
+
         self.__check_timestamp_validity()
 
     def __check_timestamp_validity(self):
         while time.clock() - self.distance_time_list[0] > self.MAX_TIME_PASSED:
             self.distance_list = self.distance_list[1:]
             self.distance_time_list = self.distance_time_list[1:]
-
 
     # Approximates the difference in speed to
     # the target by using constructing a
@@ -135,8 +143,11 @@ class Acc():
         # The incline between p0 and p1 is the relative speed
         delta_d = d1 - d0
         delta_t = t1 - t0
-        return delta_d * 100 / delta_t
 
+        if delta_d is 0 or delta_t is 0:
+            return None
+
+        return delta_d / delta_t
 
     def __get_average_of(self, my_list):
         if not len(my_list):
@@ -145,5 +156,5 @@ class Acc():
         avg = 0
         for val in my_list:
             avg = avg + val
-        avg / len(my_list)
+        avg = avg / len(my_list)
         return avg
