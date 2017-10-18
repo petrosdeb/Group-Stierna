9a47d68c2cdf70c02489016440574bfab819fe57
EmElw <wamby@student.chalmers.se>
2017-10-18 09:42:05 +0200

    debug printing in can_write.py

--- onMoped/can/interfacing/stuff/can_write.py
+++ onMoped/can/interfacing/stuff/can_write.py
@@ -1,6 +1,8 @@
 import os
 from _thread import start_new_thread
 
+import time
+
 
 class CanWriter:
     def __init__(self):
@@ -12,6 +14,7 @@ class CanWriter:
         os.system(cmd)
 
     def start_cont_send(self):
+        print("Starting continuous send")
         start_new_thread(continuous_send, (self,))  # Shorthand for sending via can-utils
 
     # if no value is given, it sends the last value to hold that value
@@ -27,6 +30,13 @@ class CanWriter:
 
 
 def continuous_send(writer):
+    last_time = time.time()
+
     while 1:
         cmd = "/home/pi/can-utils/cansend can0 '101#%02x%02x'" % (writer.out_speed, writer.out_steer)
         os.system(cmd)
+
+        c_time = int(time.time())
+        if c_time % 5 == 0 and c_time != last_time:
+            print(str(c_time) + ': continuous send active: ' + str(writer.out_speed, writer.out_steer))  # usch
+            last_time = c_time
