9eb637e9a57d350414f3cedeed671f42be354ed1
petrosdebesay <petrosd@student.chalmers.se>
2017-10-19 14:03:46 +0200

    small changes

--- onMoped/acc.py
+++ onMoped/acc.py
@@ -19,7 +19,7 @@ class Acc():
         self.distance_list = []
         self.distance_time_list = []
         self.core = core
-        drive(0)
+        self.drive(0)
         self.wanted_speed = 0
 
         self.debug_string = ""
@@ -64,7 +64,7 @@ class Acc():
                 last_time = c_time
 
             if delta_d is None or delta_v is None:
-                drive(0)
+                self.drive(0)
                 continue
 
             #set true if the car has stopped
@@ -76,11 +76,11 @@ class Acc():
             #as long as distance is less than desired distance, keep speed
             if delta_d > set_d: 
                 sp = self.wanted_speed
-                drive(sp)
+                self.drive(sp)
                 delta_d = self.__get_d()
                 '''
                 With the new distance, yet again check if the distance is larger than desired distance. 
-                Set stopped to False as the car is ready to drive
+                Set stopped to False as the car is ready to self.drive
                 '''
                 if delta_d > set_d:
                     stopped = False
@@ -107,32 +107,32 @@ class Acc():
                         print(delta_d)
                         if delta_d < 10: #Changed from 0.05 to 0.1
                             stopped = True
-                            drive(0)
+                            self.drive(0)
                 elif brake == 1:
                     distance_based_brake(self, set_d)
                 elif brake == 2:
                     decremental_brake(self, sp)
                 else:
                     sp = 0
-                    drive(0)
+                    self.drive(0)
                     
                     print(delta_d)
 
     def electric_braking(self):
         for x in range(0, 10):
             #Decrease this to improve braking. Might lead to stalling
-            drive(-100)
+            self.drive(-100)
     '''
     Slower electric brake. To avoid stopping completely
     '''
     def mod_electric_braking(self):
         for x in range(0, 10):
             #Decrease this to improve braking. Might lead to stalling
-            drive(-20)
+            self.drive(-20)
     
     def decremental_brake(self, sp):
         for x in range(0,sp):
-            drive(sp - (x + 1))
+            self.drive(sp - (x + 1))
 
     def distance_based_brake(self, set_d):
         delta_d = self.__get_d()
@@ -142,7 +142,7 @@ class Acc():
                 sp -= 5
                 if sp < 0:
                     sp = 0
-                drive(sp)
+                self.drive(sp)
                 
     def drive(self, sp):
         self.speed = sp
@@ -153,15 +153,15 @@ class Acc():
         delta_v = float(delta_v)
         # Should prevent MOPED from backing when "braking" at current speed = 0
         if previous_output == 0 and delta_v < 0:
-            drive(0)
+            self.drive(0)
         else:
             if abs(previous_output + delta_v) > 100:
                 if previous_output + delta_v < 0:
-                    drive(-100)
+                    self.drive(-100)
                 else:
-                    drive(100)
+                    self.drive(100)
             else:
-                drive(previous_output + delta_v)
+                self.drive(previous_output + delta_v)
 
     # Gets distance to preceding vehicle, if not more than 2
     def __get_d(self):
