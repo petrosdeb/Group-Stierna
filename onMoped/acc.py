c8ef5a71f9c691452690ca2059e3df14883867ac
Gustav Lahti <gustav.lahti@gmail.com>
2017-10-20 13:06:47 +0200

    Poor mans's rework to Python-branch's acc.py because we couldn't get this version of acc.py to work during the demo.

--- onMoped/acc.py
+++ onMoped/acc.py
@@ -5,10 +5,7 @@ from _thread import start_new_thread
 from math import ceil, floor
 
 
-# from acc_test import act_acc
-
-
-class Acc:
+class Acc():
     DISTANCE_CAP_METRES = 3
     DISTANCE_CAP_CENTIMETRES = DISTANCE_CAP_METRES * 100
     SAFE_DISTANCE = 40
@@ -19,7 +16,7 @@ class Acc:
         self.distance_list = []
         self.distance_time_list = []
         self.core = core
-        self.drive(0)
+        self.speed = 0
         self.wanted_speed = 0
         self.current_speed = 0
 
@@ -27,112 +24,54 @@ class Acc:
 
         start_new_thread(self.__acc_on, ())
 
-    def __acc_on(self):  # set_d, brake
-
+    def __acc_on(self):
         logging.info("Started acc_on")
 
         last_time = time.time()
-        set_d = 100
-        brake = 0
 
-        if brake != 0 or brake != 1 or brake != 2:
-            brake = 0
-            print(brake)
-
-        sp = 0
-        stopped = True
         while True:
-            
+
             delta_d = self.__get_d()
-            if delta_d == None:
-                delta_d = 0
 
             # If we have no value, set speed to 0 as a safety measure
-            
-            
 
             delta_v = self.__get_delta_v_for_forward_object()
-            '''
+
+            c_time = int(time.time())
+            if c_time % 3 == 0 and c_time != last_time:
+                logging.info("{} : {} suggest speed : {} | d_dist : {} | d_velo : {} | dbg : {}".format(
+                    c_time,
+                    type(self).__name__,
+                    self.speed,
+                    delta_d,
+                    delta_v,
+                    self.debug_string
+                ))
+                last_time = c_time
+
             if delta_d is None or delta_v is None:
-                self.drive(0)
+                self.speed = 0
                 continue
-            '''
-            # set true if the car has stopped
 
-            # as long as distance is less than desired distance, keep speed
-            if delta_d > set_d:
-                sp = self.wanted_speed
-                self.drive(sp)
-                #delta_d = self.__get_d()
-                '''
-                With the new distance, yet again check if the distance is larger than desired distance. 
-                Set stopped to False as the car is ready to self.drive
-                '''
-                if delta_d > set_d:
-                    stopped = False
-                    # time.sleep(0.1)
-                print(delta_d)
-            '''
-            if distance is smaller than desired distance, brake.
-            we have several different brake functions, depending on whether aggressive braking is needed or not
-            for aggressive braking electric brake is used, for less aggressive brake use decremental brake.
-            distance based brake aims to keep the set_d to the car in front
-            Electric brake runs the risk of stalling and reversing. Therefore using it only allows for braking
-            when distance is larger than 10 cm.
-            '''
-            if 0 <= delta_d < set_d:
-                # delta_d = self.__get_d()
-
-                if brake == 0:
-                    if not stopped:
-                        if delta_d < set_d / 2:  # added
-                            self.electric_braking()
-                        else:  # added
-                            self.mod_electric_braking()  # added
-                        print(delta_d)
-                        if delta_d < 10:  # Changed from 0.05 to 0.1
-                            stopped = True
-                            self.drive(0)
-                elif brake == 1:
-                    self.distance_based_brake(set_d)
-                elif brake == 2:
-                    self.decremental_brake(sp)
-                else:
-                    sp = 0
-                    self.drive(sp)
-
-                    print(delta_d)
-
-    def electric_braking(self):
-        for x in range(0, 10):
-            # Decrease this to improve braking. Might lead to stalling
-            self.drive(-100)
-
-    '''
-    Slower electric brake. To avoid stopping completely
-    '''
-
-    def mod_electric_braking(self):
-        for x in range(0, 10):
-            # Decrease this to improve braking. Might lead to stalling
-            self.drive(-20)
-
-    def decremental_brake(self, sp):
-        for x in range(0, sp):
-            self.drive(sp - (x + 1))
-
-    def distance_based_brake(self, set_d):
-        delta_d = self.__get_d()
-        if delta_d < set_d:
-            sp = self.core.speed
-            while sp > 0:
-                sp -= 5
-                if sp < 0:
-                    sp = 0
-                self.drive(sp)
-
-    def drive(self, sp):
-        self.speed = sp
+            if delta_d >= self.DISTANCE_CAP_CENTIMETRES:
+                self.debug_string = "NO DETECTED TARGET"
+                # in case of no obstacles, go for desired speed
+                if self.current_speed != self.wanted_speed:
+                    self.speed = self.wanted_speed
+            elif delta_d < self.SAFE_DISTANCE:  # decrease speed if too close to target
+                self.debug_string = "TOO CLOSE TO TARGET"
+                self.__set_speed(-self.wanted_speed)
+            elif delta_d > self.WANTED_DISTANCE:  # increase speed if too far away from target
+                self.debug_string = "NOT CLOSE ENOUGH TO TARGET"
+                self.__set_speed(self.wanted_speed)
+            elif delta_v < 0:  # decrease speed if moving too fast relative to target
+                self.debug_string = "TOO FAST"
+                self.__change_speed(delta_v)
+            elif delta_v > 0:  # increase speed if too slow relative to target
+                self.debug_string = "GOTTA GO FAST"
+                self.__change_speed(delta_v)
+            else:
+                self.debug_string = "NOTHING HAPPENS"
 
     def __change_speed(self, delta_v):
         delta_v = float(delta_v)
@@ -140,7 +79,6 @@ class Acc:
 
     def __set_speed(self, speed):
         # Should prevent MOPED from backing when "braking" at current speed = 0
-
         if self.current_speed is 0 and speed < 0:
             self.speed = 0
         else:
