3133218e4ebd55d0d594df03b66f5de67c0cddfa
Jesper Holm <holmje@student.chalmers.se>
2017-10-23 09:55:34 +0200

    New corrected code and files

--- lateralControl/lateralNavigation.py
+++ lateralControl/lateralNavigation.py
@@ -8,13 +8,13 @@ def __init__():
 # Param: image with a possible barcode
 # Output: Will print the calculated x-value
 def test():
-    x = getXPosition('test.jpg')
+    x=getXPosition('typ.jpg')
     print(x)
 
 def getXPosition(image):
 
     imgco = cv.LoadImage(image)
-    img = cv.CreateImage(cv.GetSize(imgco),8,1)
+    img=cv.CreateImage(cv.GetSize(imgco),8,1)
     imgx = cv.CreateImage(cv.GetSize(img),cv.IPL_DEPTH_16S,1)
     imgy = cv.CreateImage(cv.GetSize(img),cv.IPL_DEPTH_16S,1)
     thresh = cv.CreateImage(cv.GetSize(img),8,1)
@@ -23,13 +23,10 @@ def getXPosition(image):
     cv.CvtColor(imgco,img,cv.CV_BGR2GRAY)
 
     ### Finding horizontal and vertical gradient ###
-
     cv.Sobel(img,imgx,1,0,3)
     cv.Abs(imgx,imgx)
-
     cv.Sobel(img,imgy,0,1,3)
     cv.Abs(imgy,imgy)
-
     cv.Sub(imgx,imgy,imgx)
     cv.ConvertScale(imgx,img)
 
@@ -38,7 +35,6 @@ def getXPosition(image):
 
     ### Applying Threshold ###
     cv.Threshold(img,thresh,100,255,cv.CV_THRESH_BINARY)
-
     cv.Erode(thresh,thresh,None,2)
     cv.Dilate(thresh,thresh,None,5)
 
@@ -53,6 +49,8 @@ def getXPosition(image):
             bar = list(contour)
         contour=contour.h_next()
 
+    ### If the code sould be used without a graphical interface
+    ### some code below needs to be removed
     ### Draw bounding rectangles ###
     bound_rect = cv.BoundingRect(bar)
     pt1 = (bound_rect[0], bound_rect[1])
@@ -60,5 +58,8 @@ def getXPosition(image):
     cv.Rectangle(imgco, pt1, pt2, cv.CV_RGB(0,255,255), 2)
     pt = ((pt2[0]-pt1[0]),(pt2[1]-pt1[1]))
     middle = ((pt[0]/2+pt1[0]),(pt[1]/2+pt1[1]))
-    print (middle[0])
+    cv.ShowImage('img',imgco)
+    cv.WaitKey(0)
     return middle[0]
+
+test()
