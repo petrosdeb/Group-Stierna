d357326423329f90c534525325106065086115be
petrosdeb <petrosd@student.chalmers.se>
2017-10-25 16:43:28 +0200

    Not tested

--- experimental/captureimage.py
+++
@@ -1,9 +0,0 @@
-import time
-import picamera
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    time.sleep(2)
-    camera.capture('foo.jpg')
